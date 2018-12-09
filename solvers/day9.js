6591928ba9bf8f4ad24dddf2fd0bcbaabb9c6a5a
Cédric Sauriol <cedric.sauriol@hotmail.com>
2018-12-09 15:29:30 -0500

    Completed day 9.

---
+++ solvers/day9.js
@@ -0,0 +1,109 @@
+
+function Marble(value) {
+    this.value = value;
+    this.previous = undefined;
+    this.next = undefined;
+}
+
+function CircularList () {
+    this.currentMarble = new Marble(0);
+    this.currentMarble.next = this.currentMarble;
+    this.currentMarble.previous = this.currentMarble;
+}
+
+CircularList.prototype = {
+    add: function (value) {
+        let marble = new Marble(value);
+        let previousMarble = this.currentMarble.next;
+        let nextMarble = this.currentMarble.next.next;
+
+        previousMarble.next = marble;
+        marble.previous = previousMarble;
+        marble.next = nextMarble;
+        nextMarble.previous = marble;
+
+        this.currentMarble = marble;
+
+        return 0;
+    },
+
+    score: function (value) {
+        let marbleToRemove = this.currentMarble;
+        for(let i = 0; i < 7; i++) {
+            marbleToRemove = marbleToRemove.previous;
+        }
+
+        let points = marbleToRemove.value;
+
+        this.currentMarble = marbleToRemove.next;
+        marbleToRemove.previous.next = marbleToRemove.next;
+        marbleToRemove.next.previous = marbleToRemove.previous;
+
+        return points + value;
+    }
+};
+
+function initScoreboard (players) {
+    let scoreboard = {};
+
+    for (let i = 0; i < players; i++) {
+        scoreboard[i] = 0;
+    }
+
+    return scoreboard;
+}
+
+module.exports = {
+    solveA: function (input) {
+        let pattern = /(\d+) (?:.*) (\d+) (?:.*)/g;
+        let matches = pattern.exec(input);
+
+        let players = parseInt(matches[1], 10);
+        let marbleCount = parseInt(matches[2], 10);
+
+        let scoreboard = initScoreboard(players);
+        let list = new CircularList();
+
+        let score = 0;
+        let player = 0;
+        for(let value = 1; value <= marbleCount; value++) {
+            if (0 === value % 23) {
+                score = list.score(value);
+            } else {
+                score = list.add(value);
+            }
+
+            scoreboard[player] += score;
+            player = (player + 1) % players;
+        }
+
+        return Math.max(...Object.values(scoreboard));
+    },
+
+    solveB: function (input) {
+        let pattern = /(\d+) (?:.*) (\d+) (?:.*)/g;
+        let matches = pattern.exec(input);
+
+        let players = parseInt(matches[1], 10);
+        let marbleCount = parseInt(matches[2], 10) * 100;
+
+        let scoreboard = initScoreboard(players);
+        let list = new CircularList();
+
+        let score = 0;
+        let player = 0;
+        let value = 1;
+        for(value = 1; value <= marbleCount; value++) {
+            if (0 === value % 23) {
+                score = list.score(value);
+            } else {
+                score = list.add(value);
+            }
+
+            scoreboard[player] += score;
+            player = (player + 1) % players;
+        }
+
+        return Math.max(...Object.values(scoreboard));
+    }
+};
---
+++ test/testDay9.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var solver = require('../solvers/day9');
+
+describe('Day 8', function() {
+    describe('Puzzle A', function() {
+        it('Should return 32 with input 9 players; last marble is worth 25 points', function() {
+            let input = '9 players; last marble is worth 25 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 32);
+        });
+
+        it('Should return 8317 with input 10 players; last marble is worth 1618 points', function() {
+            let input = '10 players; last marble is worth 1618 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 8317);
+        });
+
+        it('Should return 146373 with input 13 players; last marble is worth 7999 points', function() {
+            let input = '13 players; last marble is worth 7999 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 146373);
+        });
+
+        it('Should return 2764 with input 17 players; last marble is worth 1104 points', function() {
+            let input = '17 players; last marble is worth 1104 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 2764);
+        });
+
+        it('Should return 54718 with input 21 players; last marble is worth 6111 points', function() {
+            let input = '21 players; last marble is worth 6111 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 54718);
+        });
+
+        it('Should return 37305 with input 30 players; last marble is worth 5807 points', function() {
+            let input = '30 players; last marble is worth 5807 points\n';
+            let result = solver.solveA(input);
+            assert.strictEqual(result, 37305);
+        });
+    });
+
+    describe('Puzzle B', function() {
+        it('Should return 107 with input 9 players; last marble is worth 1 points', function() {
+            let input = '9 players; last marble is worth 1 points\n';
+            let result = solver.solveB(input);
+            assert.strictEqual(result, 107);
+        });
+    });
+});
